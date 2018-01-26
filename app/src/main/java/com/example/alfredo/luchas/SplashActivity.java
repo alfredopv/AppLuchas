4355cb1bbc014fbef84b3a21aa64114381b4eab2
Alfredo Puente <alfredopv95@gmail.com>
2018-01-25 23:35:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alfredo/luchas/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.alfredo.luchas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+
+    private ImageView imagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imagen = (ImageView)findViewById(R.id.imageSplash);
+        imagen.setImageResource(R.drawable.lucha);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(it);
+                finish();
+            }
+        },5000);
+    }
+}
